58fb1aa61b2de03eaadcf399b16ce7a3f679f8bd
Pojkee <cbo6oda22@gmail.com>
2026-02-17 00:18:09 +0200

    made changes as requested with setter

--- app/main.py
+++ app/main.py
@@ -2,46 +2,46 @@ from __future__ import annotations
 
 
 class Animal:
-
     alive: list[Animal] = []
 
-    def __init__(
-            self,
-            name: str,
-            health: int = 100
-    ) -> None:
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.hidden = False
-        self.health = health
-        if self.health > 0:
+        self._health = health
+        if self._health > 0:
             Animal.alive.append(self)
+
+    @property
+    def health(self) -> int:
+        return self._health
+
+    @health.setter
+    def health(self, value: int) -> None:
+        if value < 0:
+            value = 0
+
+        self._health = value
+
+        if self._health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
 
-    def change_health(self, points: int) -> None:
-        self.health += points
-        if self.health <= 0 and self in Animal.alive:
-            Animal.alive.remove(self)
-
 
 class Herbivore(Animal):
-
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-
     def bite(self, herbivore: Herbivore) -> None:
-
         if not isinstance(herbivore, Herbivore):
             return
-        if herbivore.hidden:
-            # if hiding
-            print(f"{self.name} cannot bite hidden {herbivore.name}")
 
+        if herbivore.hidden:
+            print(f"{self.name} cannot bite hidden {herbivore.name}")
         else:
-            herbivore.change_health(-50)
+            herbivore.health -= 50
